330e449449a56b66db0e6c5f0620f2afb6118713
dev001hajipro <dev001hajipro@gmail.com>
2022-02-03 16:59:44 +0900

    feat: use Firebase

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,123 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { addDoc, collection, deleteDoc, doc, DocumentData, DocumentReference, onSnapshot, QuerySnapshot, setDoc, updateDoc } from 'firebase/firestore';
+import { db } from './Firebase';
+import React, { useEffect, useState } from 'react';
+
+type User = {
+  id: string
+  name: string
+  email: string
+  active: boolean
+}
 
 function App() {
+  const [title, setTitle] = useState<string>('')
+  // todo: should change generics type. like a `User[]`
+  const [users, setUsers] = useState<any[]>([])
+
+  useEffect(() => {
+    const re = collection(db, 'users')
+    const unsubscribe = onSnapshot(collection(db, 'users'), (querySnapShot: QuerySnapshot) => {
+      setUsers(querySnapShot.docs.map(v => ({ ...v.data(), id: v.id })))
+    })
+    return unsubscribe
+  }, [])
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setTitle(e.target.value)
+  }
+
+  // todo: learn react-hook-form.
+  const handleSubmit = async (e: React.SyntheticEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    const _target = e.target as typeof e.target & {
+      name: { value: string }
+      email: { value: string }
+      active: { checked: boolean }
+    }
+    console.log(_target.active.checked)
+
+    try {
+      const docRef = await addDoc(collection(db, 'users'), {
+        name: _target.name.value,
+        email: _target.email.value,
+        active: _target.active.checked
+      })
+    } catch (e) {
+      console.log("Error adding document: ", e)
+    }
+  }
+
+  const handleButton = async () => {
+    try {
+      // if you set custom Id
+      const docRef: DocumentReference<DocumentData> = doc(db, 'users', 'XXXXXABC')
+      const pm = await setDoc(docRef, {
+        name: 'by setDoc',
+        email: 'sss@ggg.com',
+        active: true
+      })
+    } catch (e) {
+      console.log("Error setDoc document: ", e)
+    }
+  }
+
+  const handleDelete = async (id: string) => {
+    await deleteDoc(doc(db, 'users', id))
+  }
+
+  const handleActive = async (e: React.ChangeEvent<HTMLInputElement>, id:string) => {
+    console.log(e.target.checked, id)
+    const docRef = doc(db, 'users', id)
+    await updateDoc(docRef, {
+      active: e.target.checked
+    })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        <label>サンプルフォーム:
+          <input type="text" onChange={handleChange} name="xx" placeholder='ダミー'></input>
+        </label>
+      </div>
+
+      <div>
+        <label>setDoc is use auto generated Id</label>
+        <button type="button" onClick={handleButton}>use setDoc</button>
+      </div>
+
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>名前:
+            <input type="text" name="name" placeholder='名前' title="名前"></input>
+          </label>
+        </div>
+        <div>
+          <label>メール:
+            <input type="email" name="email" placeholder='メール' title="メール"></input>
+          </label>
+        </div>
+        <div>
+          <label>活動中:
+            <input type="checkbox" name="active"></input>
+          </label>
+        </div>
+        <button type="submit">submit</button>
+      </form>
+
+      <div>
+        {
+          users.map(user => {
+            return (
+              <div key={user.id}>
+                <span>{user.id} {user.name} {user.email}</span>
+
+                <input type="checkbox" name="active" defaultChecked={user.active} title="is active" onChange={(e)=> handleActive(e, user.id)}></input>
+                <button type="button" onClick={() => handleDelete(user.id)}>delete</button>
+              </div>)
+          })
+        }
+      </div>
     </div>
   );
 }
---
+++ src/Firebase.tsx
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { FirebaseApp, initializeApp } from "firebase/app";
+import "firebase/firestore";
+import { getFirestore } from "firebase/firestore";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBXm7A-x3JyaOa3qQ3ZK2jAG64B7qCMjAo",
+    authDomain: "fi-todo-app2.firebaseapp.com",
+    projectId: "fi-todo-app2",
+    storageBucket: "fi-todo-app2.appspot.com",
+    messagingSenderId: "1023279041422",
+    appId: "1:1023279041422:web:1afb048d9f4f86c92b32df"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+
+console.log("XXXXX", db.app.name)
